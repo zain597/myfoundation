6302c477b49cf1a23dc7bd89aa81ccc3fdcbe407
ZainMehdi110 <zain.mehdi@rootpointers.info>
2023-06-24 23:04:41 +0500

    website theme to laravel temp

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Charifit</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href={{asset('frontend/img/favicon.png')}}>
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href={{asset('frontend/css/bootstrap.min.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/owl.carousel.min.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/magnific-popup.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/font-awesome.min.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/themify-icons.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/nice-select.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/flaticon.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/gijgo.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/animate.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/slicknav.css')}}>
+    <link rel="stylesheet" href={{asset('frontend/css/style.css')}}>
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+    @stack('css')
+</head>
+
+<body>
+
+        @include('frontend.layouts.header')
+            @yield('content')
+        @include('frontend.layouts.footer')
+
+
+
+    <!-- link that opens popup -->
+    <!-- JS here -->
+    @stack('js')
+    <script src={{asset('frontend/js/vendor/jquery-1.12.4.min.js')}}></script>
+    <script src={{asset('frontend/js/vendor/modernizr-3.5.0.min.js')}}></script>
+    <script src={{asset('frontend/js/popper.min.js')}}></script>
+    <script src={{asset('frontend/js/bootstrap.min.js')}}></script>
+    <script src={{asset('frontend/js/owl.carousel.min.js')}}></script>
+    <script src={{asset('frontend/js/isotope.pkgd.min.js')}}></script>
+    <script src={{asset('frontend/js/ajax-form.js')}}></script>
+    <script src={{asset('frontend/js/waypoints.min.js')}}></script>
+    <script src={{asset('frontend/js/jquery.counterup.min.js')}}></script>
+    <script src={{asset('frontend/js/imagesloaded.pkgd.min.js')}}></script>
+    <script src={{asset('frontend/js/scrollIt.js')}}></script>
+    <script src={{asset('frontend/js/jquery.scrollUp.min.js')}}></script>
+    <script src={{asset('frontend/js/wow.min.js')}}></script>
+    <script src={{asset('frontend/js/nice-select.min.js')}}></script>
+    <script src={{asset('frontend/js/jquery.slicknav.min.js')}}></script>
+    <script src={{asset('frontend/js/jquery.magnific-popup.min.js')}}></script>
+    <script src={{asset('frontend/js/plugins.js')}}></script>
+    <script src={{asset('frontend/js/gijgo.min.js')}}></script>
+    <!--contact js-->
+    <script src={{asset('frontend/js/contact.js')}}></script>
+    <script src={{asset('frontend/js/jquery.ajaxchimp.min.js')}}></script>
+    <script src={{asset('frontend/js/jquery.form.js')}}></script>
+    <script src={{asset('frontend/js/jquery.validate.min.js')}}></script>
+    <script src={{asset('frontend/js/mail-script.js')}}></script>
+
+    <script src={{asset('frontend/js/main.js')}}></script>
+</body>
+
+</html>
---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,80 @@
+<!-- header-start -->
+<header>
+    <div class="header-area ">
+        <div class="header-top_area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-6 col-md-12 col-lg-8">
+                        <div class="short_contact_list">
+                            <ul>
+                                <li><a href="#"> <i class="fa fa-phone"></i> +1 (454) 556-5656</a></li>
+                                <li><a href="#"> <i class="fa fa-envelope"></i>Yourmail@gmail.com</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6 col-lg-4">
+                        <div class="social_media_links d-none d-lg-block">
+                            <a href="#">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-pinterest-p"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-linkedin"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="sticky-header" class="main-header-area">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-xl-3 col-lg-3">
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="{{asset('frontend/img/logo.png')}}" alt="">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-xl-9 col-lg-9">
+                        <div class="main-menu">
+                            <nav>
+                                <ul id="navigation">
+                                    <li><a href="index.html">home</a></li>
+                                    <li><a href="About.html">About</a></li>
+                                    <li><a href="#">blog <i class="ti-angle-down"></i></a>
+                                        <ul class="submenu">
+                                            <li><a href="blog.html">blog</a></li>
+                                            <li><a href="single-blog.html">single-blog</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">pages <i class="ti-angle-down"></i></a>
+                                        <ul class="submenu">
+                                            <li><a href="elements.html">elements</a></li>
+                                            <li><a href="Cause.html">Cause</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </nav>
+                            <div class="Appointment">
+                                <div class="book_btn d-none d-lg-block">
+                                    <a data-scroll-nav='1' href="#">Make a Donate</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- header-end -->
